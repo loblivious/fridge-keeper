ad9e97938a5eff76b15aa3ba966617f30db8d762
Yen-Jung Luo <loblivious.ee@gmail.com>
2026-01-04 17:05:03 +0800

    project init

---
+++ src/main/java/com/loblivious/fridgekeeper/FridgeKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.loblivious.fridgekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeKeeperApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(FridgeKeeperApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/loblivious/fridgekeeper/FridgeKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loblivious.fridgekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeKeeperApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
